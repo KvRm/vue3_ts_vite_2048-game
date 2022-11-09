c02c458b47ccebdc8ce95f2eb4387a93090214d4
KvRm <rustamk20222@gmail.com>
2022-11-09 22:49:30 +0300

    вынес переключатель темы в отдельный компоненет

---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,62 @@
+<template>
+  <svg-icon
+      @click="switchTheme"
+      :name="`lightbulb`"
+      :width="40"
+      :height="40"
+  />
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted} from "vue";
+import SvgIcon from '../components/SvgIcon.vue'
+import {LSKeys, useLocalStorage} from "../utils/localStorage";
+
+export default defineComponent({
+  name: "ThemeSwitcher",
+  components: {
+    SvgIcon
+  },
+  
+  setup() {
+    const ls = useLocalStorage()
+
+    onMounted(() => {
+      detectColorScheme()
+    })
+
+    const switchTheme = (): void => {
+      ls.get(LSKeys.THEME) === "light" ?
+          ls.set(LSKeys.THEME, "dark") :
+          ls.set(LSKeys.THEME, "light")
+      detectColorScheme()
+    }
+
+    const detectColorScheme = (): void => {
+      const ls = useLocalStorage()
+
+      if (ls.get(LSKeys['THEME'])) {
+        ls.get(LSKeys['THEME']) === "dark" ?
+            setDataThemeAttribute("dark") :
+            setDataThemeAttribute("light")
+      } else if (window?.matchMedia("(prefers-color-scheme: light)").matches) {
+        setDataThemeAttribute("light")
+      } else {
+        setDataThemeAttribute("dark")
+      }
+    }
+
+    const setDataThemeAttribute = (theme: "dark" | "light"): void => {
+      document.documentElement.setAttribute("data-theme", theme)
+    }
+
+    return {
+      switchTheme
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,64 +1,21 @@
 <template>
   <main class="main">
-    <svg-icon
-        @click="switchTheme"
-        :name="`lightbulb`"
-        :width="40"
-        :height="40"
-    />
+    <theme-switcher/>
     <game-container/>
   </main>
 </template>
 
 <script lang="ts">
 
-import {defineComponent, onMounted} from "vue";
-import {LSKeys, useLocalStorage} from "../utils/localStorage";
+import {defineComponent} from "vue";
 import GameContainer from "../components/GameContainer.vue";
-import SvgIcon from '../components/SvgIcon.vue'
+import ThemeSwitcher from "../components/ThemeSwitcher.vue";
 
 export default defineComponent({
   name: 'MainView',
   components: {
+    ThemeSwitcher,
     GameContainer,
-    SvgIcon
-  },
-
-  setup() {
-    const ls = useLocalStorage()
-
-    onMounted(() => {
-      detectColorScheme()
-    })
-
-    const switchTheme = (): void => {
-      ls.get(LSKeys.THEME) === "light" ?
-          ls.set(LSKeys.THEME, "dark") :
-          ls.set(LSKeys.THEME, "light")
-      detectColorScheme()
-    }
-
-    const detectColorScheme = (): void => {
-      const ls = useLocalStorage()
-
-      if (ls.get(LSKeys['THEME'])) {
-        ls.get(LSKeys['THEME']) === "dark" ?
-            setDataThemeAttribute("dark") :
-            setDataThemeAttribute("light")
-      } else if (window?.matchMedia("(prefers-color-scheme: light)").matches) {
-        setDataThemeAttribute("light")
-      } else {
-        setDataThemeAttribute("dark")
-      }
-    }
-
-    const setDataThemeAttribute = (theme: "dark" | "light"): void => {
-      document.documentElement.setAttribute("data-theme", theme)
-    }
-
-    return {
-      switchTheme
-    }
   }
 })
 </script>
