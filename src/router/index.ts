6dfdfeda438872af89d42198341532e20fd8901c
KvRm <rustamk20222@gmail.com>
2022-11-19 18:17:21 +0300

    [fix] configurate routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,20 @@ import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
 	{
-		path: '/',
+		path: '/vue3_ts_vite_2048-game',
 		name: 'main',
 		component: () => import('../views/MainView.vue'),
 	},
 	{
-		path: '/404',
+		path: '/vue3_ts_vite_2048-game/404',
 		name: '404',
 		component: () => import('../views/Error404View.vue'),
 	},
 	{
-		path: '/:pathMatch(.*)',
-		redirect: '404'
+		path: '/vue3_ts_vite_2048-game/:pathMatch(.*)',
+		redirect: {
+			name: '404'
+		}
 	}
 ]
 
